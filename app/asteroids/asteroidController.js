cafc4f28e8d1fded7d7ebd8ba567c2aabe0a8e2b
Ral Oliver <raloliver.ws@gmail.com>
2017-09-16 21:34:37 -0300

    install angular moment, format date, create service, add tabs views & update controller

--- app/asteroids/asteroidController.js
+++ app/asteroids/asteroidController.js
@@ -1,22 +1,51 @@
 (function () {
     angular.module('neows').controller('asteroidController', [
         '$http',
-        '$location',
+        'moment',
+        'asteroidService',
         AsteroidController
     ])
 
-    function AsteroidController($http, $location) {
-        const vm = this
-        const url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-09-16&end_date=2017-09-16&detailed=false&api_key=N7LkblDsc5aen05FJqBQ8wU4qSdmsftwJagVK7UD'
+    function AsteroidController($http, moment, asteroidService) {
 
-        vm.getAsteroid = function () {
-            $http.get(url).then(function (response) {
-                vm.asteroid = []
-                vm.asteroids = response.data                
-            })
+        //properties
+        const vm = this
+        const momentDate = Date.now()
+        vm.asteroid = []
+        vm.asteroids = {}
+        vm.endDate = moment(momentDate).format("YYYY-MM-DD")
+        vm.endSetDate = moment(momentDate).toDate();
+        vm.startDate = moment(vm.endDate).subtract(5, "days").format("YYYY-MM-DD")
+        vm.startSetDate = moment(vm.endDate).subtract(5, "days").toDate();
+
+        //functions
+        vm.getAsteroids = function (startDate, endDate) {
+            asteroidService.getAsteroidList(startDate, endDate)
+                .then((response) => vm.updateAsteroids(response.data))
         }
 
-        vm.getAsteroid()
+        vm.updateAsteroids = function (data) {
+            vm.asteroids = data
+
+            vm.asteroid = Object.keys(vm.asteroids.near_earth_objects)
+                .map(key => {
+                    return {
+                        date: key,
+                        near_earth_objects: vm.asteroids.near_earth_objects[key]
+                    }
+                })            
+        }
+
+        //methods
+        vm.formatDate = function (format) {
+            const [year, month, day] = format.split('-');
+            const date = new Date(year, month - 1, day);
+
+            return date;
+        }
+
+        //init
+        vm.getAsteroids(vm.startDate, vm.endDate)
     }
 
 })()
