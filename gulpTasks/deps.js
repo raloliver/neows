4275a95e0859b5584e88dd12658a065befab8440
Ral Oliver <raloliver.ws@gmail.com>
2017-09-18 01:39:16 -0300

    filter date finish

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -12,9 +12,12 @@ gulp.task('deps.js', function(){
         'node_modules/angular-animate/angular-animate.min.js',            
         'node_modules/angular-ui-router/release/angular-ui-router.min.js',
         'node_modules/angular-moment/angular-moment.min.js',
+        'node_modules/angular-bootstrap-datetimepicker/src/js/datetimepicker.js',
+        'node_modules/angular-bootstrap-datetimepicker/src/js/datetimepicker.templates.js',
+        'node_modules/angular-date-time-input/src/dateTimeInput.js',
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js'
-    ]) 
+    ])
     .pipe(uglify())
     .pipe(concat('deps.min.js'))
     .pipe(gulp.dest('public/assets/js'))
@@ -23,7 +26,8 @@ gulp.task('deps.js', function(){
 gulp.task('deps.css', function(){
     gulp.src([        
         'node_modules/font-awesome/css/font-awesome.min.css',        
-        'node_modules/bootswatch/paper/bootstrap.min.css'      
+        'node_modules/bootswatch/paper/bootstrap.min.css',
+        'node_modules/angular-bootstrap-datetimepicker/src/css/datetimepicker.css'   
     ])
     .pipe(uglifycss({ "uglyComments": true }))
     .pipe(concat('deps.min.css'))
