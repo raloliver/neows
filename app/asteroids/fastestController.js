e036c722722c01777c1f84a6e892d01e73956552
Ral Oliver <raloliver.ws@gmail.com>
2017-09-18 23:38:56 -0300

    finish fastest list

--- app/asteroids/fastestController.js
+++ app/asteroids/fastestController.js
@@ -12,11 +12,8 @@
 
         vm.asteroid = []
         vm.asteroids = []
-        vm.close_approach_data = []
         vm.endDate = moment(momentDate).format("YYYY-MM-DD")
-        vm.endSetDate = moment(momentDate).toDate();
-        vm.startDate = moment(vm.endDate).subtract(5, "days").format("YYYY-MM-DD")
-        vm.startSetDate = moment(vm.endDate).subtract(5, "days").toDate()
+        vm.startDate = moment(vm.endDate).subtract(6, "days").format("YYYY-MM-DD")
 
         vm.getAsteroidsFastest = function (startDate, endDate) {
             asteroidService.getAsteroidList(startDate, endDate)
@@ -24,15 +21,26 @@
         }
 
         vm.listAsteroidsFastest = function (data) {
-            vm.asteroids = data            
-            
+            vm.asteroids = data
+
             vm.asteroid = Object.keys(vm.asteroids.near_earth_objects)
+                .sort(vm.asteroid.velocity)
                 .map(key => {
                     return {
                         date: key,
-                        near_earth_object: vm.asteroids.near_earth_objects[key]
+                        near_earth_object: vm.asteroids.near_earth_objects[key],
+                        velocity: vm.asteroids.near_earth_objects[key]
+                            .map(key => {
+                                return {
+                                    id: key.neo_reference_id,
+                                    name: key.name,
+                                    date: key.close_approach_data[0].close_approach_date,
+                                    kmh: parseInt(key.close_approach_data[0].relative_velocity.kilometers_per_hour)
+                                }
+                            })
                     }
                 })
+            
             console.log(vm.asteroid)
         }
 
--- app/common/factories/asteroidsFactory.js
+++ app/common/factories/asteroidsFactory.js
@@ -9,7 +9,7 @@
         
         function getAsteroidList(startDate, endDate) {
             return $http.get(`${consts.apiURL}/feed?start_date=${startDate}&end_date=${endDate}&detailed=false&api_key=${consts.apiKEY}`);
-        }
+        }   
         
         return {
             getAsteroidList: getAsteroidList            
