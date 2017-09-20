3856ae7c87082ec1a8292577ba0b30877e097384
Ral Oliver <raloliver.ws@gmail.com>
2017-09-20 03:46:06 -0300

    finish alerts alpha

---
+++ app/alerts/alertsController.js
@@ -0,0 +1,49 @@
+(function () {
+    angular.module('neows').controller('alertsController', [
+        '$http',
+        'asteroidService',
+        AlertsController
+    ]);
+
+    function AlertsController($http, asteroidService) {
+        var vm = this
+
+        //properties
+        vm.asteroid = {}
+        vm.asteroids = []
+
+        //functions
+        vm.getAsteroidsAlerts = function () {
+            asteroidService.getAsteroidAlerts()
+                .then((res) => vm.listAsteroidsAlerts(res.data))
+        }
+
+        vm.listAsteroidsAlerts = function (data) {
+            vm.asteroids = data
+            console.log(vm.asteroids)
+        }
+
+        //methods
+        vm.create = function () {
+            vm.asteroids.splice(0, 0, vm.asteroid)
+            vm.asteroid = ''
+        }
+
+        vm.edit = function (asteroid) {
+            vm.asteroid = vm.asteroids[asteroid]       
+            vm.editable = true
+        }
+
+        vm.update = function(){
+            vm.asteroid = {}
+            vm.editable = false            
+        };
+
+        vm.delete = function (asteroid) {
+            vm.asteroids.splice(asteroid, 1)
+        }
+
+        //init
+        vm.getAsteroidsAlerts()
+    }
+})()
